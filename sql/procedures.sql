f40602f62d3ac7d37a3b89e53bf819b666a14523
Igor do Prado Machado <igorprado1515@gmail.com>
2026-07-12 16:59:36 -0300

    feat: add first sales procedure

--- sql/procedures.sql
+++ sql/procedures.sql
@@ -1,1 +1,28 @@
 -- Stored Procedures
+
+DELIMITER $$
+
+CREATE PROCEDURE sp_registrar_item_venda(
+    IN p_venda_id INT,
+    IN p_produto_id INT,
+    IN p_quantidade INT,
+    IN p_preco_unitario DECIMAL(10,2)
+)
+BEGIN
+
+    INSERT INTO itens_venda (
+        venda_id,
+        produto_id,
+        quantidade,
+        preco_unitario
+    )
+    VALUES (
+        p_venda_id,
+        p_prodpduto_id,
+        p_quantidade,
+        p_preco_unitario
+    );
+
+END $$
+
+DELIMITER ;
