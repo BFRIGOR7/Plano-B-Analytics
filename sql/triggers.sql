9746eaaea4ff286e1d7ffedf10e64ae89d7fb806
Igor do Prado Machado <igorprado1515@gmail.com>
2026-07-12 18:56:42 -0300

    feat: add stock movement trigger

--- sql/triggers.sql
+++ sql/triggers.sql
@@ -1,1 +1,29 @@
 -- Triggers
+
+DELIMITER $$
+
+CREATE TRIGGER trg_saida_estoque_venda
+AFTER INSERT ON itens_venda
+FOR EACH ROW
+BEGIN
+
+    UPDATE produtos
+    SET estoque_atual = estoque_atual - NEW.quantidade
+    WHERE id = NEW.produto_id;
+
+    INSERT INTO movimentacoes_estoque (
+        produto_id,
+        tipo,
+        quantidade,
+        data_movimentacao
+    )
+    VALUES (
+        NEW.produto_id,
+        'SAIDA',
+        NEW.quantidade,
+        CURDATE()
+    );
+
+END $$
+
+DELIMITER ;
